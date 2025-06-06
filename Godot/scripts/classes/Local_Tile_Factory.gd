227279a7e87c325d170a6ee5ab926da9a4eacecd
Kelvin Phat <kelvinphat12@gmail.com>
2025-06-06 14:38:51 -0700

    stuff

--- Godot/scripts/classes/Local_Tile_Factory.gd
+++ Godot/scripts/classes/Local_Tile_Factory.gd
@@ -5,15 +5,10 @@ func add_photo(path : String, id : int, photo_name := "", description := "", dat
 	# load image object to get image information
 	var image = Image.load_from_file(path)
 	var new_photo_tile = PhotoTile.new(image.get_size().x, image.get_size().y, id, path)
-	new_photo_tile.description = "" # placeholder
+	new_photo_tile.description = description
 	new_photo_tile.photo_name = photo_name
 	new_photo_tile.date = date
 	
 	Database.num_of_photos += 1
 	return new_photo_tile
-	
-
-	
-func remove_photo(id : int) -> void:
-	return
 	
--- Godot/scripts/classes/Server_Tile_Factory.gd
+++ Godot/scripts/classes/Server_Tile_Factory.gd
@@ -2,14 +2,11 @@ extends TileFactory
 class_name ServerTileFactory
 
 func add_photo(path : String, id : int, photo_name := "", description := "", date := "") -> PhotoTile:
+	# load image object to get image information
 	var image = Image.load_from_file(path)
-	
 	var new_photo_tile = PhotoTile.new(image.get_size().x, image.get_size().y, id, path)
 	new_photo_tile.description = description
 	new_photo_tile.photo_name = photo_name
 	new_photo_tile.date = date
 	
 	return new_photo_tile
-	
-func remove_photo(id : int) -> void:
-	return
